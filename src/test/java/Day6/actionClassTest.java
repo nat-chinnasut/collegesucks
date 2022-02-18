92975aec6f871cdba1bd6f07069ca755c833d4db
TechCircle,LLC <53327631+techCircle@users.noreply.github.com>
2022-02-17 21:56:56 -0500

    day8 propertiesFiles

--- src/test/java/Day6/actionClassTest.java
+++ src/test/java/Day6/actionClassTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utilities.PropertiesReader;
 
 public class actionClassTest {
 	
@@ -25,7 +26,7 @@ public class actionClassTest {
 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("https://www.amazon.com/");
+		driver.get(PropertiesReader.getProperty("amazonUrl"));
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
@@ -36,7 +37,7 @@ public class actionClassTest {
 		
 		action = new Actions(driver);
 		
-		WebElement accountListButton = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span"));
+		WebElement accountListButton = driver.findElement(By.xpath(PropertiesReader.getProperty("accountListButton")));
 		
 		action.moveToElement(accountListButton).build().perform();
 		
@@ -49,13 +50,13 @@ public class actionClassTest {
 	public void dragAndDropTest() throws InterruptedException {
 		
 		
-		driver.navigate().to("http://the-internet.herokuapp.com/drag_and_drop");
+		driver.navigate().to(PropertiesReader.getProperty("dragAndDropURL"));
 		action = new Actions(driver);
 		
 		Thread.sleep(3000);
 
 		
-		action.dragAndDrop(driver.findElement(By.id("column-a")), driver.findElement(By.id("column-b"))).build().perform();
+		action.dragAndDrop(driver.findElement(By.id(PropertiesReader.getProperty("draggable"))), driver.findElement(By.id(PropertiesReader.getProperty("droppable")))).build().perform();
 		
 		
 	}
